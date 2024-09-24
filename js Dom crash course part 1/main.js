bcd31ed41cd214899da0e3b8a4b23aba0ffa8473
Bikash chaurasiya <bikash1.bay20@gmail.com>
2024-09-24 18:18:13 +0545

    Add files via upload

---
+++ js Dom crash course part 1/main.js
@@ -0,0 +1,80 @@
+// EXAMIN THE Document OBJECT//
+
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.head);
+// console.log(document.domain);
+// console.log(document.titte);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.all)
+// console.log(document.all[10]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images)
+// console.log (document.getElementById('main-header'));
+// var mainheader = document.getElementById('main-header');
+// console.log(mainheader);
+// mainheader.textContent = 'Hello';
+// mainheader.innerText = 'Goodbye';
+
+// mainheader.innerHTML = '<h3>Hello</h3>';
+
+// mainheader.style.borderBottom = 'solid 3px #000';
+// var items = document.getElementsByClassName('list-group-item');
+// // console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello2';
+// items[1].style.fontweight = 'bold';
+// items[1].style.background = 'yellow';
+
+// items.style.backgroundcolor = '#green';
+
+// var li = document.getElementsByTagName('li');
+// // console.log(items);
+// console.log(li[1]);
+
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontweight = 'bold';
+// li[1].style.background = 'yellow';
+
+
+// var header = document.querySelector('#main-header');
+// header.style.borderbottom = 'solid 4px #ccc'
+
+// var input = document.querySelector(input);
+// input.value ='Hello world';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value= "SEND";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastitem = document.querySelector
+// ('.list-group-item:last-child');
+// lastitem.style.color = 'blue';
+
+// var seconditem = document.querySelector
+// ('.list-group-item:nth-child(2)');
+// seconditem.style.color = 'green';
+
+
+// .querySelectorAll//
+
+// var titles = document.querySelectorAll('.title');
+
+// console.log (titles);
+// titles[0].textContent = 'Hello';
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
+
+// for (var i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundcolor = '#f4f4f4';
+//     even[i].style.backgroundcolor = '#ccc';
+// }
+
+
+// TARVERSING THE DOM //
